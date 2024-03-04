457c812e8c35804c689973a5bd31029326d32a10
gbopola <opeafolabi5@gmail.com>
2024-03-04 16:44:58 +0000

    change private route

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const PrivateRoute = () => {
-  const { userInfo } = useContext(UserContext);
-  return userInfo ? <Outlet /> : <Navigate to='/login' replace />;
+  const { user } = useContext(UserContext);
+  return user ? <Outlet /> : <Navigate to='/login' replace />;
 };
 export default PrivateRoute;
